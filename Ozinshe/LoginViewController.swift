a1a96c8510f7e1ea830b78fa00c157cbf4262da7
Nurasyl Melsuly <nurasylmelsuly@gmail.com>
2023-09-27 19:47:32 +0600

    Create Login VC

---
+++ Ozinshe/LoginViewController.swift
@@ -0,0 +1,86 @@
+//
+// Created for Ozinshe in 2023
+//
+
+import UIKit
+import SVProgressHUD
+import Alamofire
+import SwiftyJSON
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var emailTextField: TextFieldWithPadding!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: TextFieldWithPadding!
+    @IBOutlet weak var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureViews()
+    }
+
+    func configureViews() {
+        emailTextField.layer.borderColor = UIColor(red: 0.9, green: 0.92, blue: 0.94, alpha: 1).cgColor
+        emailTextField.layer.borderWidth = 1
+        emailTextField.layer.cornerRadius = 12
+
+        passwordTextField.layer.borderColor = UIColor(red: 0.9, green: 0.92, blue: 0.94, alpha: 1).cgColor
+        passwordTextField.layer.borderWidth = 1
+        passwordTextField.layer.cornerRadius = 12
+
+        loginButton.layer.cornerRadius = 12
+    }
+
+    @IBAction func togglePassword(_ sender: Any) {
+        passwordTextField.isSecureTextEntry.toggle()
+    }
+
+    @IBAction func forgotPasswordTouched(_ sender: Any) {
+        // TODO: Open password recovery VC
+    }
+
+    @IBAction func loginTouched(_ sender: Any) {
+        // credentials
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+
+        // validation
+        if email.isEmpty || password.isEmpty { return }
+
+        // request to API
+        SVProgressHUD.show()
+
+        let parameters = [
+            "email": email,
+            "password": password
+        ]
+
+        AF.request("http://api.ozinshe.com/auth/V1/signin", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseData { result in
+            SVProgressHUD.dismiss()
+
+            // check response body
+            if let data = result.data {
+                let json = JSON(data)
+
+                if let accessToken = json["accessToken"].string {
+                    print("Success log in!: \(accessToken)")
+                    return
+                }
+            }
+
+            // 401
+            if result.response?.statusCode == 401 {
+                SVProgressHUD.showError(withStatus: "Неправильный email или пароль!")
+                return
+            }
+
+            // any unexpected error
+            SVProgressHUD.showError(withStatus: String(data: result.data ?? Data(), encoding: .utf8))
+        }
+    }
+
+    @IBAction func registerTouched(_ sender: Any) {
+        // TODO: Open register VC
+    }
+}
+
---
+++ Ozinshe/Views/TextFieldWithPadding.swift
@@ -0,0 +1,20 @@
+//
+// Created for Ozinshe in 2023
+//
+
+import UIKit
+
+@IBDesignable
+class TextFieldWithPadding: UITextField {
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 0))
+    }
+
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 0))
+    }
+
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 0))
+    }
+}
