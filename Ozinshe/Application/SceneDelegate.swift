1be2c3245f4aad6aec3941894a8a01f82c68cb83
Nurasyl Melsuly <nurasylmelsuly@gmail.com>
2023-09-28 13:02:12 +0600

    Change back button icon & hide title

--- Ozinshe/Application/SceneDelegate.swift
+++ Ozinshe/Application/SceneDelegate.swift
@@ -11,6 +11,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
+        // navigation bar
+        configureNavBar()
+
         // show tabs if authorized
         if let accessToken = UserDefaults.standard.string(forKey: "accessToken"),
            let userEmail = UserDefaults.standard.string(forKey: "userEmail") {
@@ -31,6 +34,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // fix for SVProgressHud
         (UIApplication.shared.delegate as! AppDelegate).window = window
+    }
+
+    func configureNavBar() {
+        // hide back button title
+        let attributes = [NSAttributedString.Key.font:  UIFont(name: "Helvetica-Bold", size: 0.1)!, NSAttributedString.Key.foregroundColor: UIColor.clear]
+
+        UIBarButtonItem.appearance().setTitleTextAttributes(attributes, for: .normal)
+        UIBarButtonItem.appearance().setTitleTextAttributes(attributes, for: .highlighted)
+
+        // replace back button icon
+        UINavigationBar.appearance().backIndicatorImage = UIImage(named: "back-button")
+        UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: "back-button")
     }
 }
 
